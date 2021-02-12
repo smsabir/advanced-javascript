eb6c3307b8c2b3c90b0496576d6f965cfeb98590
S M Abdullah Al Sabir <ssabir@ttu.ee>
2021-02-12 22:29:25 +0200

    True False

---
+++ true-false.js
@@ -0,0 +1,14 @@
+/* 
+Falsy: 
+0, "", undefined, null, NaN, false.
+
+Truthy:
+'0', ' ', [], {}, true.
+*/
+let name = {};
+if(name){
+    console.log("condition is true");
+}
+else{
+    console.log("condition is false");
+};
