ee25220c257f4d26522e73dcc3bc9e0cb5086267
S M Abdullah Al Sabir <ssabir@ttu.ee>
2021-02-13 23:15:40 +0200

    scope

---
+++ scope.js
@@ -0,0 +1,9 @@
+function sum(first, second){
+    let result  = first + second;
+    return result;
+}
+console.log(result);
+const output = sum(3, 7);
+console.log(output);
+
+//scope is the are where we can access the declared variable, if we define a variable with let/const it can be only access within the function block. But var is different.
